9932388411d132c96ef1d63614c65d3ab0628b34
Maksim Bogdanov <maks.bogdanov.dev@yandex.ru>
2024-01-05 13:56:10 +0300

    feat: add common actions

---
+++ src/main/java/actions/CommonActions.java
@@ -0,0 +1,19 @@
+package actions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import waiters.BaseWaiters;
+
+public abstract class CommonActions {
+    protected WebDriver driver;
+    protected BaseWaiters waiters;
+    protected Actions actions;
+
+    protected CommonActions(WebDriver driver) {
+        this.driver = driver;
+        waiters = new BaseWaiters(driver);
+        actions = new Actions(driver);
+        PageFactory.initElements(driver, this);
+    }
+}
